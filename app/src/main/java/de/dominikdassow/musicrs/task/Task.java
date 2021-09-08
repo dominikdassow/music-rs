7b781c5381c7633ab8d971f76e16cdc2e92f0b6c
Dominik Dassow <dominik@dassow-net.de>
2021-09-08 18:06:23 +0200

    Add Task Webhook

--- app/src/main/java/de/dominikdassow/musicrs/task/Task.java
+++ app/src/main/java/de/dominikdassow/musicrs/task/Task.java
@@ -1,6 +1,20 @@
 package de.dominikdassow.musicrs.task;
 
+import de.dominikdassow.musicrs.AppConfiguration;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.core5.net.URIBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+
+import javax.net.ssl.SSLContext;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public abstract class Task {
@@ -21,6 +35,7 @@ public abstract class Task {
     }
 
     public void run() {
+        sendWebhook("Started: " + name);
         log("INIT");
         long start = System.currentTimeMillis();
 
@@ -41,6 +56,7 @@ public abstract class Task {
         }
 
         log("FINISH - " + (System.currentTimeMillis() - start) + "ms");
+        sendWebhook("Finished: " + name);
     }
 
     protected void init() throws Exception {}
@@ -51,5 +67,45 @@ public abstract class Task {
 
     private void log(String message) {
         log.trace(name + " :: " + message);
+    }
+
+    private void sendWebhook(String text) {
+        if (!AppConfiguration.get().json.hasNonNull("webhookUrl")) {
+            log.trace("No webhook url found.");
+
+            return;
+        }
+
+        try {
+            URI uri = new URIBuilder(AppConfiguration.get().json.get("webhookUrl").asText())
+                .addParameter("title", "Music RS")
+                .addParameter("text", text)
+                .build();
+
+            HttpRequest request = HttpRequest.newBuilder(uri)
+                .header("Accept", "application/json")
+                .build();
+
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, (chain, authType) -> true)
+                .build();
+
+            HttpClient client = HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .build();
+
+            client
+                .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(response -> log.trace("Sent Webhook :: " + response))
+                .get();
+        } catch (URISyntaxException |
+            InterruptedException |
+            ExecutionException |
+            NoSuchAlgorithmException |
+            KeyStoreException |
+            KeyManagementException e
+        ) {
+            e.printStackTrace();
+        }
     }
 }
